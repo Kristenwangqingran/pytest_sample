ff72cda39dee3b6cab696c99856c6b313d0c1576
Kristenwangqingran <wroolafd@gmail.com>
2024-05-28 16:53:56 +0800

    url change to self.url

--- testcase/test_falsh_sale.py
+++ testcase/test_falsh_sale.py
@@ -7,41 +7,45 @@
 @Date :23/04/2024 15:57
 """
 import requests
+import pytest
 
 
 class TestFlashSale:
+    url = "https://test.shopee.sg/api/v4/flash_sale/get_all_itemids"
+
     def test_flash_sale_invalid_promotionid(self):
-        url = "https://test.shopee.sg/api/v4/flash_sale/get_all_itemids"
         params = {
             "need_personalize": True,
             "promotionid": 23244,
             "sort_soldout": True,
             "tracker_info_version": 1
         }
-        res = requests.get(url=url, params=params)
+        res = requests.get(url=self.url, params=params)
         assert res.status_code == 200
         assert res.json()['data']['promotionid'] == None
 
     def test_flash_sale_valid_promotionid(self):
-        url = "https://test.shopee.sg/api/v4/flash_sale/get_all_itemids"
         params = {
             "need_personalize": True,
             "promotionid": 217555847557120,
             "sort_soldout": True,
             "tracker_info_version": 1
         }
-        res = requests.get(url=url, params=params)
+        res = requests.get(url=self.url, params=params)
         assert res.status_code == 200
         assert res.json()['data']['promotionid'] != None
 
     def test_flash_sale_version_error(self):
-        url = "https://test.shopee.sg/api/v4/flash_sale/get_all_itemids"
         params = {
             "need_personalize": True,
             "promotionid": 217555847557120,
             "sort_soldout": True,
             "tracker_info_version": 2
         }
-        res = requests.get(url=url, params=params)
+        res = requests.get(url=self.url, params=params)
         assert res.status_code == 200
         assert res.json()['error'] != 0
+
+
+if __name__ == "__main__":
+    pytest.main()
